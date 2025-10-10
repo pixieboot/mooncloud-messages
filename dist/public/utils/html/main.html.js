8a7673c6cbd10f7865ec439d777c4395e52bbc37
FKLR <lab_damjanovicn@protonmail.com>
2025-10-10 20:16:26 +0000

    Delete dist directory

--- dist/public/utils/html/main.html.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./bootstrap_validation.html.js");
-require("./form_validation.html.js");
-require("./show_modal.html.js");
-//# sourceMappingURL=main.html.js.map
